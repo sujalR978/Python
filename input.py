5b19c4df8ceeccee7f8559f4bdf2ab9b007e938e
sujalR978 <srashiya955@rku.ac.in>
2026-06-30 23:03:04 +0530

    #

---
+++ input.py
@@ -0,0 +1,13 @@
+"""The input() function:
+Python provides the input() built-in function to read an input from the user using the standard input device (i.e. keyboard). The input() function always returns string data irrespective of the type of data entered through the keyboard.
+
+Syntax: var_name = input([“interactive statement”])
+
+where,
+
+var_name is the variable assigned with the string value which is read using input method.
+
+Interactive statement is the statement displayed to the user expecting the response from them."""
+
+input_var = input("Enter the value : ")
+print(input_var)
