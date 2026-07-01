d0d55d2eac7e58fc6e96da55a5837a4aa549878c
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:21:00 +0530

    #

--- Exersise.py
+++ Exersise.py
@@ -8,3 +8,4 @@ Write a Python program that displays a message as follows for a given number:
 - If it is a multiple of both 3 and 5, display "Zoom"
 
 - If it does not satisfy any of the above given conditions, display 'Invalid'"""
+
