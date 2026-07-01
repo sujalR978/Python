f2245de02db9fc4f25c9d767c46a9f1b6d61bff7
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:14:35 +0530

    #

---
+++ if-else.py
@@ -0,0 +1,12 @@
+"""if-else:
+It is a conditional statement used for selection between two set of statements based on the evaluation of test condition. The statements inside the if block are executed only if the evaluated condition is True. Otherwise statements inside the else block are executed. As we have two set of statements to select based on the test condition, it is also called as two-way selection statement."""
+
+# https://infyspringboard.onwingspan.com/common-content-store/Shared/Shared/Public/lex_auth_012656924899647488303_shared/web-hosted/assets/ifelse.png
+
+
+
+a=-10
+if(a>0):
+	print("positive integer")
+else:
+	print("Not a positive integer")
