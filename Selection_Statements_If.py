f666a10483b252ecfc3beac6c5fbb366f1cf6148
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:12:50 +0530

    #

---
+++ Selection_Statements_If.py
@@ -0,0 +1,12 @@
+"""During the execution of the program, we may not wish to execute all set of statements sequentially. Sometimes, we may wish to select between various set of statements based on some conditions. Depending on the test condition evaluation, the flow is determined within the program. Let’s have look at few of them.
+
+if (simple if):
+It is a conditional statement used for decision making in Python. In if statement, the test condition is evaluated and the statements inside the if block are executed only if the evaluated condition is True. In the below if statement, we have only one set of statements to select based on the test condition. So, it is also called as one-way selection statement."""
+
+# https://infyspringboard.onwingspan.com/common-content-store/Shared/Shared/Public/lex_auth_012656924899647488303_shared/web-hosted/assets/if.png
+
+
+
+a=10
+if(a>0):
+     print("positive integer")
