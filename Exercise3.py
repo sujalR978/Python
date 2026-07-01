1d289fa18a177cab7fcfeff61ea3923c619ae457
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 16:00:15 +0530

    #

---
+++ Exercise3.py
@@ -0,0 +1,31 @@
+"""Write a Python program to check whether the given year is leap year or not.
+
+Write a Python program to find and display the maximum of three given numbers.
+
+Write a Python program to check the given number is prime number or not.
+
+Write a Python program to print first n Fibonacci numbers.
+
+An organization has decided to provide salary hike to its employees based on their job level. Employees can be in job levels 3 , 4 or 5. Hike percentage based on job levels are given below:
+
+Job level
+
+Hike Percentage (applicable on current salary)
+
+3
+
+15
+
+4
+
+7
+
+5
+
+5
+
+ 
+
+In case of invalid job level, consider hike percentage to be 0.
+
+Given the current salary and job level, write a Python program to find and display the new salary of an employee"""
