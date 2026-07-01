8a6d0c7cbbf646677d5420c0101253bd6dd04451
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 15:39:11 +0530

    #

---
+++ Exercise.py
@@ -0,0 +1,5 @@
+"""Write a Python program to find the sum of digits of a given number.
+
+Example: Sum of digits of the number 123 will be 6
+
+Note: Initialize the number with various values and test your program."""
