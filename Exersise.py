3008e0e8b7a3535c98fc03e853d74ac5a0892f8c
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:20:02 +0530

    #

---
+++ Exersise.py
@@ -0,0 +1,10 @@
+"""Exercise
+Write a Python program that displays a message as follows for a given number:
+
+If it is a multiple of 3, display "Zip"
+
+If it is a multiple of 5, display "Zap"
+
+If it is a multiple of both 3 and 5, display "Zoom"
+
+If it does not satisfy any of the above given conditions, display 'Invalid'"""
