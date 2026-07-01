e1ae9785a4968cc67b82349fb142d19626303584
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 15:27:35 +0530

    #

---
+++ observation.py
@@ -0,0 +1,22 @@
+for number in range(1,5):
+    print ("The current number is ",number)
+print("---------------------------")
+for number in range(1,7,2):
+    print ("The current number is ",number)
+print("---------------------------")
+for number in range(5,0,-1):
+    print ("The current number is ",number)
+"""The current number is  1
+The current number is  2
+The current number is  3
+The current number is  4
+---------------------------
+The current number is  1
+The current number is  3
+The current number is  5
+---------------------------
+The current number is  5
+The current number is  4
+The current number is  3
+The current number is  2
+The current number is  1"""
