93d6e153e152a0d32d690ea2032b59c6a814599a
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:10:00 +0530

    #

--- Control_Structures.py
+++ Control_Structures.py
@@ -1,4 +1,5 @@
 """Control Structures are used to control the flow of execution in a Python program.
  Sometimes, it is required to skip execution of few statements based on the logic and 
  in some cases, a set of statements may be needed to get executed repeatedly for a finite 
- number of times or based on the value of the variable."""
+#  number of times or based on the value of the variable."""
+# https://infyspringboard.onwingspan.com/common-content-store/Shared/Shared/Public/lex_auth_012656924899647488303_shared/web-hosted/assets/control_structures.png
