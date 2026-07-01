f49845f7158fec7731ae16da175646c1da4477b7
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:29:56 +0530

    #

---
+++ for_loop.py
@@ -0,0 +1,8 @@
+"""for loop:
+In Python, the for loop allows the loop to run over a specific sequence of values. In other words, for every value in the sequence, the loop runs once. Thus, we can avoid infinite loops by using a for loop."""
+
+# https://infyspringboard.onwingspan.com/common-content-store/Shared/Shared/Public/lex_auth_012656924899647488303_shared/web-hosted/assets/for.png
+
+
+for number in 1,2,3,4,5:
+    print("The current number is ",number)
