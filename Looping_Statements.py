e817fc880ba1491096c915cf0f9afebb1e227a1c
sujalR978 <srashiya955@rku.ac.in>
2026-07-01 11:28:43 +0530

    #

---
+++ Looping_Statements.py
@@ -0,0 +1,14 @@
+"""Looping statements are used to execute the same block of code multiple times in Python based on the test condition. Let’s have look some of the looping statements in Python.
+
+while loop:
+        The while loop is used to execute a piece of code as long as the test condition is True. The while loop is preferred whenever the number of iterations is not known.
+
+Syntax:"""
+
+# https://infyspringboard.onwingspan.com/common-content-store/Shared/Shared/Public/lex_auth_012656924899647488303_shared/web-hosted/assets/while.png
+
+num=5
+count=1
+while count <= num:
+    print("the current number is:",count)
+    count+=1
